8849e8711a8ae37f1c4c1e5569c7bc91b6109305
Sashaank Ramakrishnan <sashaankrk@gmail.com>
2024-07-13 18:10:50 +0530

    simple chakra button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import { Button, ButtonGroup } from "@chakra-ui/react";
 
 const App = () => {
-  return <div>App</div>;
+  return <Button colorScheme="blue">Button</Button>;
 };
 
 export default App;
