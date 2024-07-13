7dd78f2a9955b9a448054fe2d454aed707acf097
Sashaank Ramakrishnan <sashaankrk@gmail.com>
2024-07-13 18:05:15 +0530

    deleting all for the final game

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,7 @@
-import axios, { CanceledError } from "axios";
-import { useEffect, useState } from "react";
+import React from "react";
 
-interface User {
-  id: number;
-  name: string;
-}
-
-function App() {
-  const [users, setUsers] = useState<User[]>([]);
-  const [error, setError] = useState("");
-  const [isLoading, setLoading] = useState(false);
-
-  useEffect(() => {
-    const controller = new AbortController();
-    setLoading(true);
-    axios
-      .get<User[]>("https://jsonplaceholder.typicode.com/users")
-      .then((res) => {
-        setLoading(false);
-        setUsers(res.data);
-      })
-      .catch((err) => {
-        if (err instanceof CanceledError) return;
-        setError(err.message);
-        setLoading(false);
-      });
-    return () => controller.abort();
-  }, []);
-
-  return (
-    <>
-      {error && <p className="text-danger">{error}</p>}
-      {isLoading && <div className="spinner-border"></div>}
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-    </>
-  );
-}
+const App = () => {
+  return <div>App</div>;
+};
 
 export default App;
